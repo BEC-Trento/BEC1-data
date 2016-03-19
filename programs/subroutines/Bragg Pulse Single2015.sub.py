a1c44095650e5a22f7d8e8685ebd55b45f4bbc5e
stronzio <stronzio@stronzio-pc>
2016-03-19 20:01:41 +0000

    20160319 Bragg reboot

--- programs/subroutines/Bragg Pulse Single2015.sub.py
+++ programs/subroutines/Bragg Pulse Single2015.sub.py
@@ -1,4 +1,4 @@
-prg_comment=""
+prg_comment = ""
 prg_version = "0.5.1"
 def program(prg, cmd):
     prg.add(-20000000, "Na Bragg (+) freq", 110.00)
@@ -6,13 +6,13 @@ def program(prg, cmd):
     prg.add(-19200000, "Na 3D MOT cool (+) freq", 150.00)
     prg.add(-19000000, "Na Bragg (-) freq", 110.00)
     prg.add(-4000000, "Shutter Bragg Open")
+    prg.add(-3900000, "Bragg OFF")
     prg.add(-500000, "Shutter Bragg Close")
-    prg.add(-20000, "Bragg OFF")
     prg.add(-6000, "Na Bragg", 104)
-    prg.add(-2200, "Na 3D MOT cool (+) freq", 110.00)
-    prg.add(-1800, "Na 3D MOT cool (-) freq", 110.00)
-    prg.add(-1400, "Na 3D MOT cool (+) Amp", 300)
-    prg.add(-1000, "Na 3D MOT cool (-) Amp", 300)
+    prg.add(-2200, "Na 3D MOT cool (+) freq", 110.50)
+    prg.add(-1800, "Na 3D MOT cool (-) freq", 109.50)
+    prg.add(-1400, "Na 3D MOT cool (+) Amp", 1000)
+    prg.add(-1000, "Na 3D MOT cool (-) Amp", 1000)
     prg.add(0, "Bragg ON")
     prg.add(250000, "Bragg OFF")
     prg.add(310750, "Na 3D MOT cool (-) Amp", 1)
