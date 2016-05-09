9fca2dbcc04aac35633095ae246c8d53d78f3bde
stronzio <stronzio@stronzio-pc>
2016-05-09 17:43:59 +0100

    20160509 New Bragg actions

--- programs/subroutines/Bragg Pulse testing.py
+++ programs/subroutines/Bragg Pulse testing.py
@@ -5,9 +5,10 @@ def program(prg, cmd):
     prg.add(5000, "Shutter Bragg Open")
     prg.add(6000, "Shutter Probe Na Open", enable=False)
     prg.add(23000000, "Bragg Pulse Single2015.sub", enable=False)
+    prg.add(23000000, "Bragg Pulse Single New_20160509.sub")
+    prg.add(23060000, "Picture NaK Bragg New_20160509.sub")
     prg.add(23060000, "Picture NaK Bragg Tom.sub", enable=False)
-    prg.add(23060000, "Picture NaK.sub")
-    prg.add(23060000, "Picture NaK Bragg.sub", enable=False)
+    prg.add(23060000, "Picture NaK.sub", enable=False)
     return prg
 def commands(cmd):
     from numpy import arange
