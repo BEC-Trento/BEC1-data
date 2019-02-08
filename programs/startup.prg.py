64027c997d46aabf589572f98b9c935db68094c2
bec1 <carmelo.mordini@unitn.it>
2019-02-08 23:59:01 +0100

    Auto-commit at 2019-02-08-T235901

--- programs/startup.prg.py
+++ programs/startup.prg.py
@@ -12,3 +12,23 @@ def program(prg, cmd):
     prg.add(2000000, "Dark Spot MOT load.sub")
     prg.add(2100000, "Config MOT.sub")
     return prg
+def commands(cmd):
+    import numpy as np
+    rep1 = np.arange(0,6,1)
+    uw1 = [0.7, 1.8, 2.5]
+    rep_arr, uw_tau_arr = np.meshgrid(rep1, uw1)
+    iters = list(zip(rep_arr.ravel(), uw_tau_arr.ravel()))
+    np.random.shuffle(iters)
+    j = 0
+    while(cmd.running):
+        print('\n-------o-------')
+        rep1, uw_tau1 = iters[j]
+        cmd.set_var('rep', rep1)
+        cmd.set_var('uw_tau', uw_tau1)
+        print('\n')
+        print('Run #%d/%d, with variables:\nrep = %g\nuw_tau = %g\n'%(j+1, len(iters), rep1, uw_tau1))
+        cmd.run(wait_end=True, add_time=35000)
+        j += 1
+        if j == len(iters):
+            cmd.stop()
+    return cmd
