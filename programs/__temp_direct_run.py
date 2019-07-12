f9ec98dbefde3655b45b5d801723d474d185331c
bec1 <carmelo.mordini@unitn.it>
2019-07-12 23:59:02 +0200

    Auto-commit at 2019-07-12-T235901

--- programs/__temp_direct_run.py
+++ programs/__temp_direct_run.py
@@ -1,5 +1,5 @@
 prg_comment = ""
 prg_version = "0.7"
 def program(prg, cmd):
-    prg.add(0, "TTL2 4 OFF")
+    prg.add(0, "Na 3D MOT cool (-) Amp", 100)
     return prg
--- programs/__temp_iter.py
+++ programs/__temp_iter.py
@@ -11,6 +11,7 @@ def program(prg, cmd):
     prg.add(199980000, "Scope 1 Trigger OFF")
     prg.add(199985000, "Scope 2 Trigger ON")
     prg.add(199986000, "Scope 2 Trigger OFF")
+    prg.add(199991500, "Optical pumping", enable=False)
     prg.add(200000000, "Load_Quad")
     prg.add(200010000, "Quad_RampUP")
     prg.add(200030000, "Evaporation ramp", start_t=0.0000, func_args="a=40e6, b=4e6, duration=10, tau=1", n_points=500, func="(b - a * exp(-duration / tau) + (a - b) * exp(-t / tau)) / (1 - exp(-duration / tau))", stop_t=5000.0000, functions=dict(func_args=lambda x: 'a={}, b={}, duration={}, tau={}'.format(cmd.get_var('evap1_fstart')*1e6, cmd.get_var('evap1_fend')*1e6, cmd.get_var('evap1_time')*1e-3, cmd.get_var('evap1_tau')), stop_t=lambda x: cmd.get_var('evap1_time')))
@@ -21,7 +22,7 @@ def program(prg, cmd):
     prg.add(231010000, "Config MOT.sub", functions=dict(time=lambda x: 20000+cmd.get_var('evap1_time')+cmd.get_var('evap2_time')+cmd.get_var('Quad_rampdown_time')*0), enable=False)
     prg.add(232001000, "Config Field OFF.sub", functions=dict(time=lambda x: 20000+cmd.get_var('evap1_time')+cmd.get_var('evap2_time')+cmd.get_var('Quad_rampdown_time')*0+cmd.get_var('hold_time')))
     prg.add(232051000, "BEC_imaging", functions=dict(time=lambda x: 20000+cmd.get_var('evap1_time')+cmd.get_var('evap2_time')+cmd.get_var('Quad_rampdown_time')*0+cmd.get_var('hold_time')+cmd.get_var('tof')))
-    prg.add(258575000, "DarkSpotMOT_19.sub", functions=dict(time=lambda x: 20000+cmd.get_var('evap1_time')+cmd.get_var('evap2_time')+cmd.get_var('Quad_rampdown_time')*0+cmd.get_var('hold_time')+cmd.get_var('tof')+1000), enable=False)
+    prg.add(258575000, "DarkSpotMOT_19.sub", functions=dict(time=lambda x: 20000+cmd.get_var('evap1_time')+cmd.get_var('evap2_time')+cmd.get_var('Quad_rampdown_time')*0+cmd.get_var('hold_time')+cmd.get_var('tof')+1000))
     prg.add(259990000, "Mirror x RIGHT Out", enable=False)
     prg.add(259990000, "Mirror z BOTTOM Out", enable=False)
     return prg
