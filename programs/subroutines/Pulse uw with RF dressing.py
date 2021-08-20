8ee9a6f0eaee53f23f2ab4ef41aaa5abbee2be28
bec1 <carmelo.mordini@unitn.it>
2021-08-20 23:59:02 +0200

    Auto-commit at 2021-08-20-T235901

--- programs/subroutines/Pulse uw with RF dressing.py
+++ programs/subroutines/Pulse uw with RF dressing.py
@@ -2,13 +2,13 @@ prg_comment = ""
 prg_version = "0.7"
 def program(prg, cmd):
     prg.add(-1000, "RF_freq", 109, functions=dict(frequency=lambda x: cmd.get_var('RF_freq')*1e6+cmd.get_var('RF_freq_delta')*1e3))
+    prg.add(-1000, "RF_freq", 0, functions=dict(frequency=lambda x: cmd.get_var('RF_freq_m1')*1e6+cmd.get_var('RF_freq_m1_delta')*1e3), enable=False)
     prg.add(-400, "RF_amp", 1000, functions=dict(amplitude=lambda x: cmd.get_var('RF_amp')))
     prg.add(0, "Pulse uw", polarity=1, pulse_t=0.00200, functions=dict(pulse_t=lambda x: 1e-3*cmd.get_var('marconi1_pulsetime')))
-    prg.add(110, "RF_freq", 110, functions=dict(frequency=lambda x: cmd.get_var('RF_freq2')*1e6-cmd.get_var('RF_freq_delta2')*1e3, time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')))
+    prg.add(110, "RF_freq", 110, functions=dict(frequency=lambda x: cmd.get_var('RF_freq2')*1e6-cmd.get_var('RF_freq_delta')*1e3, time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')))
     prg.add(700, "Pulse uw", polarity=1, pulse_t=0.00200, functions=dict(pulse_t=lambda x: 1e-3*cmd.get_var('marconi1_pulsetime2'), time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')))
     prg.add(800, "RF_amp", 100, functions=dict(amplitude=lambda x: cmd.get_var('RF_amp_dressing'), time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')+1e-3*cmd.get_var('marconi1_pulsetime2')))
     prg.add(1500, "RF_freq", 109, functions=dict(frequency=lambda x: cmd.get_var('RF_freq_dressing')*1e6, time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')+1e-3*cmd.get_var('marconi1_pulsetime2')))
-    prg.add(2500, "Pulse uw", polarity=1, pulse_t=0.00200, functions=dict(pulse_t=lambda x: cmd.get_var('marconi1_pulsetime_dressing')*1e-3+cmd.get_var('dipole_hold3_time')-0.1, time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')+1e-3*cmd.get_var('marconi1_pulsetime2')))
-    prg.add(2600, "RF_amp", 1, functions=dict(time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')+1e-3*cmd.get_var('marconi1_pulsetime2')+cmd.get_var('dipole_hold3_time')+1e-3*cmd.get_var('marconi1_pulsetime_dressing')+cmd.get_var('mix_time')), enable=False)
-    prg.add(3200, "RF_freq", 60, functions=dict(time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')+1e-3*cmd.get_var('marconi1_pulsetime2')+cmd.get_var('dipole_hold3_time')+1e-3*cmd.get_var('marconi1_pulsetime_dressing')+cmd.get_var('mix_time')), enable=False)
+    prg.add(2500, "Pulse uw", polarity=1, pulse_t=0.00200, functions=dict(pulse_t=lambda x: cmd.get_var('marconi1_pulsetime_dressing')*1e-3+cmd.get_var('dipole_mix_time')+cmd.get_var('dipole_hold3_time')-0.1, time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')+1e-3*cmd.get_var('marconi1_pulsetime2')))
+    prg.add(3000, "Probe y AOM TTL long ", functions=dict(time=lambda x: x+1e-3*cmd.get_var('marconi1_pulsetime')+1e-3*cmd.get_var('marconi1_pulsetime2')), enable=False)
     return prg
--- programs/subroutines/Ramp_bias_field.py
+++ programs/subroutines/Ramp_bias_field.py
@@ -1,7 +1,9 @@
 prg_comment = ""
 prg_version = "0.7"
 def program(prg, cmd):
+    prg.add(-55000, "IGBT B grad x ON")
     prg.add(0, "B comp x ramp", start_t=0, stop_x=0, n_points=100, start_x=0, stop_t=50, functions=dict(stop_x=lambda x: cmd.get_var('Bx_dipole_trap'), start_x=lambda x: cmd.get_var('Bx_GM')+cmd.get_var('Bx_bottom')))
-    prg.add(10000, "B comp y ramp", start_t=0, stop_x=0, n_points=100, start_x=0, stop_t=50, functions=dict(start_x=lambda x: cmd.get_var('By_GM')+cmd.get_var('By_bottom'), stop_x=lambda x: cmd.get_var('By_dipole_trap')))
-    prg.add(20000, "B comp z ramp", start_t=0.0000, stop_x=0.000, n_points=100, start_x=1.000, stop_t=50.0000, functions=dict(start_x=lambda x: cmd.get_var('Bz_GM')+cmd.get_var('Bz_bottom'), stop_x=lambda x: cmd.get_var('Bz_dipole_trap')))
+    prg.add(10000, "B comp y ramp", start_t=0, stop_x=0, n_points=100, start_x=0, stop_t=50, functions=dict(stop_x=lambda x: cmd.get_var('By_dipole_trap'), start_x=lambda x: cmd.get_var('By_GM')+cmd.get_var('By_bottom')))
+    prg.add(20000, "B comp z ramp", start_t=0.0000, stop_x=0.000, n_points=100, start_x=1.000, stop_t=50.0000, functions=dict(stop_x=lambda x: cmd.get_var('Bz_dipole_trap'), start_x=lambda x: cmd.get_var('Bz_GM')+cmd.get_var('Bz_bottom')))
+    prg.add(30000, "B grad x ramp", start_t=0, stop_x=0, n_points=100, start_x=0, stop_t=50, functions=dict(stop_x=lambda x: cmd.get_var('Bx_grad')))
     return prg
